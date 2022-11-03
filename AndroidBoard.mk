71836e34d23f5d1e4f2e9e881ada26d2bbc01407
congying <quic_congying@quicinc.com>
2022-11-03 18:56:56 +0800

    anorak: Add back keycode for Kernel to Android key mapping
    Add back keycode for Kernel to Android key mapping in vendor code
    to support Halliday QXR physical back key.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -28,6 +28,16 @@ endif
 #----------------------------------------------------------------------
 # Copy additional target-specific files
 #----------------------------------------------------------------------
+# Add gpio-keys.kl for anorak
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := gpio-keys.kl
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/usr/keylayout
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional
